548a15836a9e5540bb90c38722387d1ebc179fd8
Ryanaltair <al_tair@live.com>
2024-11-20 23:46:55 +0800

    threshold

--- shaders/shader.glsl
+++ shaders/shader.glsl
@@ -8,9 +8,15 @@ uniform vec2 uSize;
 uniform sampler2D uTexture;
 
 out vec4 fragColor;
-
+const float threshold = 0.5;//阈值
 void main() {
     vec2 coo = FlutterFragCoord().xy / uSize;
-    vec4 color = texture(uTexture, coo);
-    fragColor = color;
+    vec4 color = texture(uTexture,coo);
+    
+    float r = color.r;
+    float g = color.g;
+    float b = color.b;
+    g = r * 0.3 + g * 0.59 + b * 0.11;
+    g = g <= threshold ? 0.0 : 1.0;
+    fragColor = vec4(g, g, g, 1.0);
 }

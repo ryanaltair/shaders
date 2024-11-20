5ffa530d8c28411dd5230231699f0efad53f1261
Ryanaltair <al_tair@live.com>
2024-11-20 23:45:55 +0800

    draw image

--- lib/pyramid.dart
+++ lib/pyramid.dart
@@ -1,4 +1,5 @@
-import 'dart:ui';
+import 'dart:async';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
@@ -10,18 +11,36 @@ class Pyramid extends StatefulWidget {
 }
 
 class _PyramidState extends State<Pyramid> {
-  FragmentShader? _shader;
-
+  ui.FragmentShader? _shader;
+  ui.Image? _image;
   @override
   void initState() {
     super.initState();
     _loadShader();
   }
 
+  static Future<ui.Image> loadImageByProvider(
+    ImageProvider provider, {
+    ImageConfiguration config = ImageConfiguration.empty,
+  }) async {
+    Completer<ui.Image> completer = Completer<ui.Image>(); //完成的回调
+    late ImageStreamListener listener;
+    ImageStream stream = provider.resolve(config); //获取图片流
+    listener = ImageStreamListener((ImageInfo frame, bool sync) {
+//监听
+      final ui.Image image = frame.image;
+      completer.complete(image); //完成
+      stream.removeListener(listener); //移除监听
+    });
+    stream.addListener(listener); //添加监听
+    return completer.future; //返回
+  }
+
   @override
   Widget build(BuildContext context) {
-    final shader=_shader;
-    if (shader == null) {
+    final shader = _shader;
+    final image = _image;
+    if (shader == null || image == null) {
       return const Center(
         child: CircularProgressIndicator(),
       );
@@ -29,26 +48,31 @@ class _PyramidState extends State<Pyramid> {
     return Center(
       child: CustomPaint(
         size: const Size(500 * 0.8, 658 * 0.8),
-        painter: ShaderPainter(  shader: shader  ),
+        painter: ShaderPainter(shader: shader, image: image),
       ),
     );
   }
 
   void _loadShader() async {
     String path = 'shaders/shader.glsl';
-    FragmentProgram program = await FragmentProgram.fromAsset(path);
+    ui.FragmentProgram program = await ui.FragmentProgram.fromAsset(path);
     _shader = program.fragmentShader();
+    _image = await loadImageByProvider(AssetImage('assets/test.jpeg'));
     setState(() {});
   }
 }
 
 class ShaderPainter extends CustomPainter {
-  ShaderPainter({required this.shader});
+  ShaderPainter({required this.shader, required this.image});
 
-  FragmentShader shader;
+  ui.FragmentShader shader;
+  ui.Image image;
 
   @override
   void paint(Canvas canvas, Size size) {
+    shader.setFloat(0, size.width);
+    shader.setFloat(1, size.height);
+    shader.setImageSampler(0, image);
     final paint = Paint()..shader = shader;
     canvas.drawRect(
       Rect.fromLTWH(0, 0, size.width, size.height),
@@ -57,5 +81,7 @@ class ShaderPainter extends CustomPainter {
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
--- shaders/shader.glsl
+++ shaders/shader.glsl
@@ -1,10 +1,16 @@
 #version 460 core
 
 precision mediump float;
+
+#include <flutter/runtime_effect.glsl>
+
+uniform vec2 uSize;
+uniform sampler2D uTexture;
+
 out vec4 fragColor;
 
-vec3 blue = vec3(5, 83, 177) / 255;
-
 void main() {
-  fragColor = vec4(blue, 1);
+    vec2 coo = FlutterFragCoord().xy / uSize;
+    vec4 color = texture(uTexture, coo);
+    fragColor = color;
 }
